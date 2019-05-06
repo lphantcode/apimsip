f97a56c598e0332b7d383ce73fc5f4c5cc8b996d
adaypr <38984581+adaypr@users.noreply.github.com>
2019-05-06 09:55:12 +0200

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+// settings
+app.set('port', process.env.PORT || 3000 );
+
+// middlewares
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+
+// routers
+require('./routes/QuotationMockUpRoutes')(app);
+
+app.listen(app.get('port'), () => {
+   console.log('server on port 3000');
+});
