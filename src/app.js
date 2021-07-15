19a6ecf3952d0e411a0e0370556c0c8562d1a942
adaypr <38984581+adaypr@users.noreply.github.com>
2021-07-15 23:50:17 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,8 +1,5 @@
 const express = require('express');
 const app = express();
-const path = require('path');
-app.set("views", path.join(__dirname))
-app.set("view engine", "ejs")
 
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
