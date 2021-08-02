97eeec95493f0fa4e84d350a9ef10618836f44c5
adaypr <38984581+adaypr@users.noreply.github.com>
2021-08-02 12:55:00 +0200

    Update QuotationMockUpRoutes.js

--- src/routes/QuotationMockUpRoutes.js
+++ src/routes/QuotationMockUpRoutes.js
@@ -54,7 +54,7 @@ module.exports = function (app) {
 			    "ContactDetails": null
 			},
 		    {
-		      "Id": "bbbbbb-cccc-4dddd-eeei-aaaaaaaaaaaaa",
+		      "Id": "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
 		      "TenantId": "aaa1112223333",
 		      "TenantOrigin": "bbbbbbbbbb123456",
 		      "OperatorId": "987654321cbd",
